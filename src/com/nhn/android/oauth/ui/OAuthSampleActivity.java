ac544c0a1598f4fa58a9d10bd0760d2797bab809
heonyu <hypositive@gmail.com>
2015-05-07 14:17:09 +0900

    다시 작업

--- src/com/nhn/android/oauth/ui/OAuthSampleActivity.java
+++ src/com/nhn/android/oauth/ui/OAuthSampleActivity.java
@@ -33,7 +33,7 @@ public class OAuthSampleActivity extends Activity {
 	private static final String TAG = "OAuthSampleActivity";
 	
 	private static final String TAG2 = "git Test";
-	private static final String TAG3 = "오리지널";
+	private static final String TAG3 = "오리지널111";
 	/**
 	 * client 정보를 넣어준다.
 	 */
