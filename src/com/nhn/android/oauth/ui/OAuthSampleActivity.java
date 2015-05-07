af2d4bc892e60f330ceb0324f6c0eb4f38e76c11
test <testttttttt@hanmail.net>
2015-05-07 13:53:11 +0900

    첫번째 컴잇

--- src/com/nhn/android/oauth/ui/OAuthSampleActivity.java
+++ src/com/nhn/android/oauth/ui/OAuthSampleActivity.java
@@ -34,7 +34,7 @@ public class OAuthSampleActivity extends Activity {
 	private static final String TAG1 = "OAuthSampleActivity1111111111111";
 	
 	private static final String TAG2 = "git Test";
-	private static final String TAG3 = "테스트 2번을 거쳐갑니다.";
+	private static final String TAG3 = "테스트 3333333번을 거쳐갑니다.";
 	private static final String TAG4 = "2233113131312ffbffg33";
 	/**
 	 * client 정보를 넣어준다.
