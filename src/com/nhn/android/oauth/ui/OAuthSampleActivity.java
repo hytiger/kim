fe5d29781cbf0b7fd160db86ab58af32553184c4
rrobbie <hypositive@gmail.com>
2015-05-07 16:39:11 +0900

    1

--- src/com/nhn/android/oauth/ui/OAuthSampleActivity.java
+++ src/com/nhn/android/oauth/ui/OAuthSampleActivity.java
@@ -32,7 +32,7 @@ public class OAuthSampleActivity extends Activity {
 
 	private static final String TAG = "OAuthSampleActivity";
 	
-	private static final String TAG2 = "git Test121313";
+	private static final String TAG2 = "git Tes2525t121313";
 	private static final String TAG3 = "테33111fffffeff13422341113";
 	/**
 	 * client 정보를 넣어준다.eeeeeee1111111
