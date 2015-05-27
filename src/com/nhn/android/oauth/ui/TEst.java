a1c519874dc7b4e03a9f1d93ad578c291908cad1
heonyu <hypositive@gmail.com>
2015-05-27 17:42:41 +0900

    111111111111111111111

--- src/com/nhn/android/oauth/ui/TEst.java
+++ src/com/nhn/android/oauth/ui/TEst.java
@@ -3,5 +3,20 @@ package com.nhn.android.oauth.ui;
 public class TEst {
 	
 	String text = "";
+	String text1;
+
+	
+	public TEst() {
+	}
+
+	public TEst(String text, String text1) {
+		super();
+		this.text = text;
+		this.text1 = text1;
+	}
+	
+	
+	
+	
 
 }
