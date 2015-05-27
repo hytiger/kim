6e0d270a3f69aecf3e2d3784ae86ac3bfbcb0cf8
heonyu <hypositive@gmail.com>
2015-05-27 13:48:48 +0900

    good 22222

--- src/com/nhn/android/oauth/ui/OAuthSampleActivity.java
+++ src/com/nhn/android/oauth/ui/OAuthSampleActivity.java
@@ -1,24 +1,8 @@
 package com.nhn.android.oauth.ui;
 
-import java.io.StringReader;
-
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserFactory;
-
 import android.app.Activity;
-import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.nhn.android.naverlogin.OAuthLogin;
-import com.nhn.android.naverlogin.OAuthLoginDefine;
-import com.nhn.android.naverlogin.OAuthLoginHandler;
-import com.nhn.android.naverlogin.ui.view.OAuthLoginButton;
 import com.nhn.android.oauth.R;
 
 public class OAuthSampleActivity extends Activity {
